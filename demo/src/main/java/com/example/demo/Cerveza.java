338cfc9be4c0188da7eb751cda63222eb8b72959
papiness77 <80712587+papiness77@users.noreply.github.com>
2021-11-17 17:42:26 -0300

    Update Cerveza.java

--- demo/src/main/java/com/example/demo/Cerveza.java
+++ demo/src/main/java/com/example/demo/Cerveza.java
@@ -4,11 +4,26 @@ public class Cerveza extends BebidaAlcoholica{
 
     private int ibu;
     private String color;
-    
+
     public Cerveza(String color,int ibu,String descripcion, String nombre, String marca, String tipo, int cantidad, int precio, boolean alcohol, boolean artesanal, int graduacion){
         super(descripcion, nombre, marca, tipo, cantidad, precio, alcohol, artesanal, graduacion);
         this.color = color;
         this.ibu = ibu;
     }
 
+    public int getIbu() {
+        return ibu;
+    }
+
+    public void setIbu(int ibu) {
+        this.ibu = ibu;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
