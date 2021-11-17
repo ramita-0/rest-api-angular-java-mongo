b236d98f33375a98b854cb2cdcb67f985f18afc6
papiness77 <80712587+papiness77@users.noreply.github.com>
2021-11-17 17:43:04 -0300

    Update BebidaNoAlcoholica.java

--- demo/src/main/java/com/example/demo/BebidaNoAlcoholica.java
+++ demo/src/main/java/com/example/demo/BebidaNoAlcoholica.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class BebidaNoAlcoholica extends Bebida{
 
-    public BebidaNoAlcoholica(String nombre, String marca, String tipo, int cantidad, int precio, String descripcion){
-        super(nombre, marca, tipo, cantidad, precio, descripcion);
+    public BebidaNoAlcoholica(String nombre, String marca, String tipo, int cantidad, int precio, String descripcion,boolean alcohol){
+        super(nombre, marca, tipo, cantidad, precio, descripcion,alcohol);
     }
 }
