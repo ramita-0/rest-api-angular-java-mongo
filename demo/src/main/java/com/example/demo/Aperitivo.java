5ee296f1bf39b33844a0604a4b3951c8f1b46501
papiness77 <80712587+papiness77@users.noreply.github.com>
2021-11-17 17:44:36 -0300

    Update Aperitivo.java

--- demo/src/main/java/com/example/demo/Aperitivo.java
+++ demo/src/main/java/com/example/demo/Aperitivo.java
@@ -1,8 +1,8 @@
 package com.example.demo;
 
 public class Aperitivo extends BebidaAlcoholica{
-
-    public Aperitivo(String descripcion, String nombre, String marca, String tipo, int cantidad, int precio, boolean alcohol, boolean artesanal, int graduacion){
+    public Aperitivo(String descripcion, String nombre, String marca, String tipo, int cantidad, int precio, boolean alcohol, boolean artesanal, int graduacion) {
         super(descripcion, nombre, marca, tipo, cantidad, precio, alcohol, artesanal, graduacion);
     }
+
 }
